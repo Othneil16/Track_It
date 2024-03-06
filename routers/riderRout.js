142c118f86aa2bfafa77e748f93295e23471235e
Othneil16 <othneilvictory16@gmail.com>
2024-03-06 12:35:46 +0100

    update

--- routers/riderRout.js
+++ routers/riderRout.js
@@ -6,7 +6,7 @@ const multer = require('../imagesutils/multer');
 
 const riderRouter = require("express").Router()
 
-riderRouter.post('/create', multer.single('profileImage'),companyauthenticate, createRider)
+riderRouter.post('/company/rider/create', multer.single('profileImage'),companyauthenticate, createRider)
 
 // riderRouter.post('/company/rider/SignUp', companyauthenticate, createRider)
 
