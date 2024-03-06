8a717e7f83b16e128c5f0b4b8107f00380fd3d32
Othneil16 <othneilvictory16@gmail.com>
2024-03-06 17:24:22 +0100

    update

--- controllers/companyCont.js
+++ controllers/companyCont.js
@@ -178,7 +178,7 @@ exports.companyVerifyEmail = async (req, res) => {
     // if(company.isVerified === true){
     //  return res.status(200).send(generateLoginCss, `<script>setTimeout(()=>{window.location.href = 'https://the-track.it.vercel.app/login' ;}, 2000); </script>`);
     // }
-        res.redirect( `https://the-track-it.vercel.app/login` );
+        res.redirect( `https://the-track-it.vercel.app/companylogin` );
      
     } catch (error) {
       res.status(500).json({
