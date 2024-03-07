19623e3f99da16eb3ccce36c62830e53a8c4f71f
Othneil16 <othneilvictory16@gmail.com>
2024-03-07 01:05:21 +0100

    update

--- controllers/riderCont.js
+++ controllers/riderCont.js
@@ -21,7 +21,7 @@ const transporter = nodemailer.createTransport({
  
 // Function to generate unique ID
 const generateUniqueId = (length)=> {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = '0123456789';
     let result = '';
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
